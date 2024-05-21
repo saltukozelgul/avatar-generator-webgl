8765f9b2cd2965215878c956179afb32e624dd91
saltukozelgul <saltukozelgul@gmail.com>
2024-05-21 18:45:04 +0300

    feat: basic layout with tailwind

---
+++ js/tailwind_conf.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    purge: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+      },
+    },
+  },
+};
